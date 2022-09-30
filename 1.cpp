1164e990c097d3f55eafcb8289f0b63af34c0d31
nitishkrish16 <107739446+nitishkrish16@users.noreply.github.com>
2022-09-30 09:03:00 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class splay_tree
+{
+public:
+     int data;
+     splay_tree *left;
+     splay_tree *right;
+};
+void insert(int );
+void remove(int);
+splay_tree* splay(splay_tree*);
+
+
+void insert(int data)
+{
+
+}
+
+
+int main()
+{
+
+}
+
